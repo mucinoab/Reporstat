3854b1521057c58b2b8e46ce350b07bb3965ad57
Documenter.jl <documenter@juliadocs.github.io>
2021-01-05 02:55:41 +0000

    build based on b3c1bf6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Covid.jl","page":"Home","title":"Covid.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Covid.jl","category":"page"},{"location":"#Functions-Documentation","page":"Home","title":"Functions Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"func(x)","category":"page"},{"location":"#Covid.func-Tuple{Any}","page":"Home","title":"Covid.func","text":"func(x)\n\nReturns double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+}
