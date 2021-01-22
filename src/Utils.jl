244e02894c36114bf0aefac5b46a188aef9c1131
Bruno A. Muciño <mucinoab@gmail.com>
2021-01-22 11:13:02 -0600

    Remplaza la estructura población por DataFrame

--- src/Utils.jl
+++ src/Utils.jl
@@ -2,8 +2,7 @@ push!(LOAD_PATH,"../src/")
 using Dates, Printf
 
 using InfoZIP, HTTP, DataFrames, CSV, StringEncodings, JSON
-export unzip, data_check, fechahoy, sumacolumna, sumafila, jsonparse, poblacion_mexico, poblacion_entidad, poblacion_municipio, poblacion_todos_municipios
-export poblacion
+export unzip, data_check, fechahoy, sumacolumna, sumafila, poblacion_mexico, poblacion_entidad, poblacion_municipio, poblacion_todos_municipios
 
 include("Constants.jl") # Diccionario de entidades y municipios 
 
@@ -30,7 +29,7 @@ end
 """
     CSV_to_DataFrame(path_url::String, encoding::String="UTF-8")::DataFrame
 
-Lee un archivo CSV con el `encoding` indicado y regresa un _DataFrame_.
+Lee un archivo CSV con el `encoding` indicado y regresa un `DataFrame`.
 
 # Ejemplo
 ```julia-repl
@@ -59,7 +58,7 @@ end
 """
     data_check(path_url::String, type::String="PATH", encoding::String="UTF-8")::DataFrame
 
-Crea un _DataFrame_ dado un archivo CSV o una liga al archivo.
+Crea un `DataFrame` dado un archivo CSV o una liga al archivo.
 Se pude especificar el _encoding_.
 
 # Ejemplo
@@ -100,7 +99,7 @@ end
     sumacolumna(tabla::DataFrame, col::Int)::Number
     sumacolumna(tabla::DataFrame, col::String)::Number
 
-Suma todos los valores de una determinada columna en un DataFrame.
+Suma todos los valores de una determinada columna en un `DataFrame`.
 Para hacer referencia a que columna se desea sumar se pude usar la posición de la columna o el nombre que tiene.
 
 # Ejemplo
@@ -129,7 +128,7 @@ end
 """
     sumafila(tabla::DataFrame, fila::Int)::Number
 
-Suma todos los valores de una determinada fila en un DataFrame.
+Suma todos los valores de una determinada fila en un `DataFrame`.
 La fila se especifica con la posición en la que se encuentra.
 
 # Ejemplo
@@ -189,21 +188,10 @@ function token_check(token_INEGI::String)::String
   return token_INEGI
 end
 
-struct poblacion
-  lugar::String
-  total::Float64
-  hombres::Float64
-  mujeres::Float64
-  porcentaje_hombres::Float64
-  porcentaje_mujeres::Float64
-  porcentaje_indigena::Float64
-  densidad_poblacion::Float64
-end
-
 """
-    poblacion_mexico(token_INEGI::String="")::poblacion
+    poblacion_mexico(token_INEGI::String="")::DataFrame
 
-Regresa un una estructura `poblacion` con los datos más recientes, a nivel nacional, proporcionados por la API de Indicadores del INEGI.
+Regresa un `DataFrame` con los datos más recientes, a nivel nacional, proporcionados por la API de Indicadores del INEGI.
 Requiere el token (`token_INEGI`) de la API, puede obtenerse [aquí.](https://www.inegi.org.mx/app/api/indicadores/interna_v1_1/tokenVerify.aspx)
 Se pude proporcionar el token directamente o por medio de una variable de entorno llamada de la misma manera, `token_INEGI`.
 
@@ -213,15 +201,14 @@ julia> ENV["token_INEGI"] = "00000000-0000-0000-0000-000000000000"
 "00000000-0000-0000-0000-000000000000"
 
 julia> popu = poblacion_mexico()
-México
-
-Población total 119938473.00 (60.96 hab/km²)
-Hombres 54855231.00 (48.57%)
-Mujeres 57481307.00 (51.43%)
-Porcentaje que se considera indígena 21.50%
+1×8 DataFrame
+ Row │ lugar   total      hombres    mujeres    porcentaje_hombres  porcentaje_mujeres  porcentaje_indigena  densidad
+     │ String  Float64    Float64    Float64    Float64             Float64             Float64              Float64
+─────┼────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │ México  1.19938e8  5.48552e7  5.74813e7               48.57               51.43              21.4965   60.9642
 ```
 """
-function poblacion_mexico(token_INEGI::String="")::poblacion
+function poblacion_mexico(token_INEGI::String="")::DataFrame
   token_INEGI = token_check(token_INEGI)
   url = "https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/6207019014,1002000001,1002000002,1002000003,6207020032,6207020033,3105001001/es/0700/true/BISE/2.0/"*token_INEGI*"?type=json"
   return parse_poblacion(jsonparse(url), "México")
@@ -230,17 +217,18 @@ end
 """
     poblacion_entidad(cve_entidad::String, token_INEGI::String="")::poblacion
 
-Regresa un una estructura `poblacion` con los datos más recientes, por entidad federativa, proporcionados por la API de Indicadores del INEGI.
+Regresa un una `DataFrame` con los datos más recientes, por entidad federativa, proporcionados por la API de Indicadores del INEGI.
 Requiere el token (`token_INEGI`) de la API, puede obtenerse [aquí.](https://www.inegi.org.mx/app/api/indicadores/interna_v1_1/tokenVerify.aspx)
 Se pude proporcionar el token directamente o por medio de una variable de entorno, de la siguiente manera. 
 
 ```julia-repl
 julia> ENV["token_INEGI"] = "00000000-0000-0000-0000-000000000000"
 ```
+El `DataFrame` resultante contiene los siguientes datos.
 
-La estructura _poblacion_  contiene los siguientes datos.
 - lugar
 - población total
+- densidad de población (habitantes por kilómetro cuadrado) 
 - población total hombres
 - población total mujeres
 - porcentaje de hombres
@@ -264,29 +252,15 @@ La estructura _poblacion_  contiene los siguientes datos.
 
 # Ejemplo
 ```julia-repl
-julia> struct poblacion
-  lugar::String
-  total::Float64
-  hombres::Float64
-  mujeres::Float64
-  porcentaje_hombres::Float64
-  porcentaje_mujeres::Float64
-  porcentaje_indigena::Float64
-end
-
 julia> popu = poblacion_entidad("31", token)
-Yucatán
-
-Población total 2102259.00 (53.06 hab/km²)
-Hombres 963333.00 (49.00%)
-Mujeres 992244.00 (51.00%)
-Porcentaje que se considera indígena 65.40%
-
-julia> popu.porcentaje_indigena
-65.403459
+1×8 DataFrame
+ Row │ lugar    total      hombres   mujeres   porcentaje_hombres  porcentaje_mujeres  porcentaje_indigena  densidad
+     │ String   Float64    Float64   Float64   Float64             Float64             Float64              Float64
+─────┼───────────────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │ Yucatán  2.10226e6  963333.0  992244.0             48.9968             51.0032              65.4035   53.0607
 ```
 """
-function poblacion_entidad(cve_entidad::String, token_INEGI::String="")::poblacion
+function poblacion_entidad(cve_entidad::String, token_INEGI::String="")::DataFrame
   token_INEGI = token_check(token_INEGI)
   try
     global lugar = entidades[cve_entidad]
@@ -299,9 +273,9 @@ function poblacion_entidad(cve_entidad::String, token_INEGI::String="")::poblaci
 end
 
 """
-    poblacion_municipio(cve_entidad::String, cve_municipio::String, token_INEGI::String="")::poblacion
+    poblacion_municipio(cve_entidad::String, cve_municipio::String, token_INEGI::String="")::DataFrame
 
-Regresa un una estructura `poblacion` con los datos más recientes, por municipio, proporcionados por la API de Indicadores del INEGI.
+Regresa un `DataFrame` con los datos más recientes, por municipio, proporcionados por la API de Indicadores del INEGI.
 Requiere el token (`token_INEGI`) de la API, puede obtenerse [aquí.](https://www.inegi.org.mx/app/api/indicadores/interna_v1_1/tokenVerify.aspx)
 Se pude proporcionar el token directamente o por medio de una variable de entorno llamada de la misma manera, `token_INEGI`.
 
@@ -326,15 +300,14 @@ julia> ENV["token_INEGI"] = "00000000-0000-0000-0000-000000000000"
 "00000000-0000-0000-0000-000000000000"
 
 julia> popu = poblacion_municipio("01", "002")
-Aguascalientes, Asientos
-
-Población total 45492.00 (84.63 hab/km²)
-Hombres 22512.00 (48.95%)
-Mujeres 22980.00 (51.05%)
-Porcentaje que se considera indígena 3.64%
+1×8 DataFrame
+ Row │ lugar                     total    hombres  mujeres  porcentaje_hombres  porcentaje_mujeres  porcentaje_indigena  densidad
+     │ String                    Float64  Float64  Float64  Float64             Float64             Float64              Float64
+─────┼────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │ Aguascalientes, Asientos  45492.0  22512.0  22980.0             48.9519             51.0481              3.63938   84.6325
 ```
 """
-function poblacion_municipio(cve_entidad::String, cve_municipio::String, token_INEGI::String="")::poblacion
+function poblacion_municipio(cve_entidad::String, cve_municipio::String, token_INEGI::String="")::DataFrame
   token_INEGI = token_check(token_INEGI)
   try
     global estado = entidades[cve_entidad]
@@ -350,7 +323,6 @@ function poblacion_municipio(cve_entidad::String, cve_municipio::String, token_I
 
   url = "https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000002,1002000003,6207019014,6207020032,6207020033,3105001001/es/070000"*cve_entidad*"0"*cve_municipio*"/true/BISE/2.0/"*token_INEGI*"?type=json"
 
-
   datos = jsonparse(url)
   indicadores = Dict{String, Float64}()
 
@@ -359,19 +331,23 @@ function poblacion_municipio(cve_entidad::String, cve_municipio::String, token_I
   end
 
   lugar = estado * ", " * municipio
-  hombres = indicadores["1002000002"]             
-  mujeres = indicadores["1002000003"]             
-  total = hombres + mujeres #Parce ser que el API no proporciona este dato(!?)
-  densidad = indicadores["3105001001"]            
-  porcentaje_hombres = indicadores["6207020032"]  
-  porcentaje_mujeres = indicadores["6207020033"]  
-  porcentaje_indigena = indicadores["6207019014"] 
+  hom = indicadores["1002000002"]       
+  muj = indicadores["1002000003"]       
+  tot = hom + muj #Parce ser que el API no proporciona este dato(!?)
+  den = indicadores["3105001001"]       
+  porcen_hom = indicadores["6207020032"]
+  porcen_muj = indicadores["6207020033"]
+  porcen_ind = indicadores["6207019014"]
 
-  return poblacion(lugar, total, hombres, mujeres, porcentaje_hombres, porcentaje_mujeres, porcentaje_indigena, densidad)
+  df = DataFrame(lugar=[lugar], total=[tot], hombres=[hom], mujeres=[muj],
+    porcentaje_hombres=[porcen_hom], porcentaje_mujeres=[porcen_muj], 
+    porcentaje_indigena=[porcen_ind], densidad_poblacion=[den])
+  
+  return df 
 end
 
 #TODO documentación
-function parse_poblacion(datos::Dict, lugar::String)::poblacion
+function parse_poblacion(datos::Dict, lugar::String)::DataFrame
   
   indicadores = Dict{String, Float64}()
 
@@ -384,53 +360,59 @@ function parse_poblacion(datos::Dict, lugar::String)::poblacion
   # densdad = 3105001001 (hab/km^2) porhom = 6207020032 pormuj = 6207020033
   # indígena= 6207019014 
 
-  total = indicadores["1002000001"]               # población total                                 
-  hombres = indicadores["1002000002"]             # población hombres
-  mujeres = indicadores["1002000003"]             # población mujeres
-  densidad = indicadores["3105001001"]            # densidad de población
-  porcentaje_hombres = indicadores["6207020032"]  # porcentaje de hombres
-  porcentaje_mujeres = indicadores["6207020033"]  # porcentaje de mujeres
-  porcentaje_indigena = indicadores["6207019014"] # porcentaje de población que se considera indígena
+  tot = indicadores["1002000001"]        # población total                                 
+  hom = indicadores["1002000002"]        # población hombres
+  muj = indicadores["1002000003"]        # población mujeres
+  den = indicadores["3105001001"]        # densidad de población
+  porcen_hom = indicadores["6207020032"] # porcentaje de hombres
+  porcen_muj = indicadores["6207020033"] # porcentaje de mujeres
+  porcen_ind = indicadores["6207019014"] # porcentaje de población que se considera indígena
 
-  return poblacion(lugar, total, hombres, mujeres, porcentaje_hombres, porcentaje_mujeres, porcentaje_indigena, densidad)
-end
-
-#Sobrecarga la manera en la que la estructura poblacion se imprime.
-function Base.show(io::IO, ::MIME"text/plain", p::poblacion)
-    compact = get(io, :compact, false)
-    msg = Printf.@sprintf("%s\n\nPoblación total %.02f (%.02f hab/km²)\nHombres %.02f (%.02f%%)\nMujeres %.02f (%.02f%%)\nPorcentaje que se considera indígena %.02f%%", 
-                          p.lugar, p.total, p.densidad_poblacion, p.hombres, p.porcentaje_hombres, p.mujeres, p.porcentaje_mujeres, p.porcentaje_indigena)
-    println(io, msg)
+  df = DataFrame(lugar=[lugar], total=[tot], hombres=[hom], mujeres=[muj],
+    porcentaje_hombres=[porcen_hom], porcentaje_mujeres=[porcen_muj], 
+    porcentaje_indigena=[porcen_ind], densidad_poblacion=[den])
+  
+  return df 
 end
 
 """
-    poblacion_todos_municipios()::Vector{poblacion}
+    poblacion_todos_municipios()::DataFrame
 
-Regresa un vector de `poblacion` con los datos de _todos_ los municipios.
+Regresa un `DataFrame` con los datos poblacionales de _todos_ los municipios.
+
+- nombre del lugar
+- clave de entidad 
+- nombre de la entidad
+- clave de municipio
+- nombre de municipio
+- población total
+- densidad de población (habitantes por kilómetro cuadrado) 
+- población total hombres
+- población total mujeres
+- porcentaje de hombres
+- porcentaje de mujeres
+- porcentaje de población que se considera indígena
 
 # Ejemplo
 ```julia-repl
 julia> poblacion_todos_municipios()
-poblacion("Aguascalientes, Aguascalientes", 797010.0, 744.58015, 386429.0, 410581.0, 48.533499, 51.466501, 11.87029)
-poblacion("Aguascalientes, Asientos", 797010.0, 744.58015, 386429.0, 410581.0, 48.533499, 51.466501, 11.87029)
-⋮ 
-poblacion("Zacatecas, Trancoso", 138176.0, 331.02637, 66297.0, 71879.0, 48.482008, 51.517992, 8.3546019)
-poblacion("Zacatecas, Santa María de la Paz", 16934.0, 87.919183, 8358.0, 8576.0, 48.962036, 51.037964, 5.223304)
+2469×11 DataFrame
+  Row │ entidad  entidad_nombre  municipio  municipio_nombre              total     densidad   hombres   mujeres   porcentaje_hombres  porcentajes_mujeres ⋯
+      │ String   String          String     String                        Float64   Float64    Float64   Float64   Float64             Float64             ⋯
+──────┼───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+    1 │ 01       Aguascalientes  001        Aguascalientes                797010.0  744.58     386429.0  410581.0             48.5335              51.4665 ⋯
+    2 │ 01       Aguascalientes  002        Asientos                      797010.0  744.58     386429.0  410581.0             48.5335              51.4665 
+    ⋮   │    ⋮           ⋮             ⋮                   ⋮                   ⋮          ⋮         ⋮         ⋮              ⋮                    ⋮          ⋱
+ 2468 │ 32       Zacatecas       057        Trancoso                      138176.0  331.026     66297.0   71879.0             48.482               51.518  ⋯
+ 2469 │ 32       Zacatecas       058        Santa María de la Paz          16934.0   87.9192     8358.0    8576.0             48.962               51.038  
 ```
 """
-function poblacion_todos_municipios()::Vector{poblacion}
-  munis =  Vector{poblacion}()
-  sizehint!(munis, 2469)
-
+function poblacion_todos_municipios()::DataFrame
   path = "muni.csv"
   if !isfile(path)
     global path = HTTP.download("https://raw.githubusercontent.com/mucinoab/mucinoab.github.io/dev/extras/muni.csv", pwd())
   end
 
-  df = CSV.File(path, types=[String, String, String, String, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64])
-
-  for r in df
-    push!(munis, poblacion(r[2] * ", " * r[4], r[5], r[7], r[8], r[9], r[10], r[11], r[6]))
-  end
-  return munis
+  return DataFrame(CSV.File(path, types=[String, String, String, String, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64]))
 end
+
