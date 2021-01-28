8079567841f557697a7a1575a3bcfe601eee48b2
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-01-28 17:13:16 -0600

    arreglar doc

--- src/Covid.jl
+++ src/Covid.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH,"../src/")
 module Covid
 
 #include("Stats.jl") #incluimos los archivos con las funciones
+include("Filter.jl")
 include("Utils.jl")
 include("Operations.jl")
-include("Filter.jl")
 end
